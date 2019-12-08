194765d3cbf3c7fd66a221d1e4ad9b72a308a203
wonde <wondwosen.shi@gmail.com>
2019-12-08 23:20:45 +0100

    Updates

---
+++ precache-manifest.e2589ba6bddf03254f0ce991831a5a09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a5c43a575972097abbc",
+    "url": "/shoppingcart/static/js/runtime~main.a3ba74b3.js"
+  },
+  {
+    "revision": "80788d22f69d0ed8a1fe",
+    "url": "/shoppingcart/static/js/main.e21ae57b.chunk.js"
+  },
+  {
+    "revision": "182a70bd899862789057",
+    "url": "/shoppingcart/static/js/2.388127c1.chunk.js"
+  },
+  {
+    "revision": "80788d22f69d0ed8a1fe",
+    "url": "/shoppingcart/static/css/main.15038f86.chunk.css"
+  },
+  {
+    "revision": "182a70bd899862789057",
+    "url": "/shoppingcart/static/css/2.ef6363de.chunk.css"
+  },
+  {
+    "revision": "b787964131f9ee386389889a58459588",
+    "url": "/shoppingcart/index.html"
+  }
+];
